766d9e142ce1823a109a1e3b9efa0929a7e1a311
joemyson-santos <joemyson1402@gmail.com>
2020-09-13 17:17:21 -0300

    iniciando com .Net

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrimeiroProjeto_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"bem vindo ao curso de .Net {i}");
+
+            }
+        }
+    }
+}
